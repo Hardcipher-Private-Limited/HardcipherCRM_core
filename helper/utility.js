16d8e14ccf81e0870206bbda0e3d731b0b6bfad5
AAbhishek18 <94705002+AAbhishek18@users.noreply.github.com>
2023-05-29 22:04:51 +0530

    update

--- helper/utility.js
+++ helper/utility.js
@@ -34,32 +34,3 @@ module.exports = {
   generateSerialNumber,
 };
 
-// const fs = require("fs");
-// const SERIAL_NUMBER_FILE = "serial_number.txt";
-// let currentNumber;
-
-// function initializeSerialNumber() {
-//   try {
-//     const serialNumber = fs.readFileSync(SERIAL_NUMBER_FILE, "utf8");
-//     currentNumber = parseInt(serialNumber, 10);
-//     if (isNaN(currentNumber)) {
-//       throw new Error("Invalid serial number data");
-//     }
-//   } catch (err) {
-//     currentNumber = 0;
-//   }
-// }
-
-// function generateSerialNumber() {
-//   currentNumber++;
-//   const serialNumber = currentNumber.toString().padStart(4, "0");
-//   fs.writeFileSync(SERIAL_NUMBER_FILE, currentNumber.toString(), "utf8");
-//   return serialNumber;
-// }
-
-// // Initialize the serial number on server start
-// initializeSerialNumber();
-
-// module.exports = {
-//   generateSerialNumber,
-// };
